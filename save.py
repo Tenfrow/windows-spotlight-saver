ebc85999bde3f2fe4dc572c1fcac5a8752fc857c
Tenfrow <vit2zremb@gmail.com>
2018-04-18 20:05:48 +0300

    Refactor and add docs

--- save.py
+++ save.py
@@ -4,48 +4,66 @@ from PIL import Image
 from shutil import copyfile
 from time import time
 
-source_path = os.path.join(os.getenv('LOCALAPPDATA'),
+SOURCE_PATH = os.path.join(os.getenv('LOCALAPPDATA'),
                            r'Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets')
 
-dest_path = sys.argv[1] if len(sys.argv) > 1 else os.path.join(os.path.dirname(os.path.realpath(__file__)), 'spotlight')
+DEST_PATH = sys.argv[1] if (len(sys.argv) > 1) else os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                                 'spotlight')
 
-if not os.path.exists(dest_path):
-    os.mkdir(dest_path)
+if not os.path.exists(DEST_PATH):
+    os.mkdir(DEST_PATH)
 
 
 def main():
-    saved_files_filename = 'saved_files.txt'
-    if not os.path.isfile(saved_files_filename):
-        open(saved_files_filename, 'w').close()
-    with open(saved_files_filename) as f:
-        saved_images = f.read().splitlines()
-    source_images = os.listdir(source_path)
-    new_images = filter_img_items(set(source_images) - set(saved_images))
-    save_as_jpg(new_images)
-    with open(saved_files_filename, 'a+') as f:
-        f.write('\n'.join(new_images) + '\n')
-
-
-def full_path(filename):
-    return os.path.join(source_path, filename)
-
-
-def filter_img_items(items):
-    def is_fullhd(filename):
-        with Image.open(full_path(filename)) as img:
-            return (1920, 1080) == img.size
-
-    return [i for i in items if is_fullhd(i)]
-
-
-def save_as_jpg(items):
-    saved_count = len(items)
-    for i in items:
+    saved_count = 0
+    for filename in os.listdir(SOURCE_PATH):
+        file_path = os.path.join(SOURCE_PATH, filename)
         try:
-            copyfile(full_path(i), os.path.join(dest_path, str(time())) + '.jpg')
-        except OSError:
-            saved_count -= 1
-    print(f'{saved_count} images has been saved to {dest_path}')
+            if is_fullhd(file_path) and is_unique(file_path):
+                save_as_jpg(file_path, DEST_PATH)
+                saved_count += 1
+        except Exception as e:
+            print(e)
+    print(f'{saved_count} new images have been saved to {DEST_PATH}')
+    input("Press Enter to exit")
+
+
+def save_as_jpg(file_path, destination):
+    """
+    :param file_path: full path of the file to be saved
+    :param destination: destination folder
+    """
+    copyfile(file_path, os.path.join(destination, str(time())) + '.jpg')
+
+
+def is_fullhd(filename):
+    """
+    Returns True if the image is 1920 by 1080 pixels
+    :param filename: full path of the file to be checked
+    :rtype: bool
+    """
+    with Image.open(filename) as img:
+        return img.size == (1920, 1080)
+
+
+saved_files = set()
+
+
+def is_unique(filename):
+    """
+    This check uses only comparison by size of images so it is not perfect,
+    but for this task it should be enough to guarantee uniqueness.
+    :param filename: Full path of the file to be checked
+    :rtype: bool
+    """
+    global saved_files
+
+    if not saved_files:
+        saved_files = set(map(lambda fname: os.stat(os.path.join(DEST_PATH, fname)).st_size, os.listdir(DEST_PATH)))
+
+    if os.stat(filename).st_size in saved_files:
+        return False
+    return True
 
 
 if __name__ == "__main__":
